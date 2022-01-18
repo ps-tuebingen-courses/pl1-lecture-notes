6d181abe77019af49cb76609f0cf1e27049db23d
BinderDavid <binderd@informatik.uni-tuebingen.de>
2022-01-18 18:30:53 +0100

    Fix 2 (#3)
    * Create bash file
    
    * Add some comments to script

---
+++ create_scala_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script can be used for generating *.scala files from the *.md files in the
+# ./src directory.
+#
+# The script runs on all markdown files in the ./src directory and deletes
+# everything which isn't inbetween scala codefences, e.g.
+#
+# ```scala
+# val x = 1;
+# ```
+#
+echo "Creating .scala files..."
+for d in $(find ./src -name "*.md" -type f)
+do
+  echo "Create .scala file for $d..."
+  sed -n '/```scala/,/```/{/```\(scala\)\?/n;p}' $d > "$(dirname $d)/$(basename -- "$d" .md).scala"
+done
