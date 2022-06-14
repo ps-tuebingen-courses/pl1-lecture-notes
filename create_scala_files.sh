0d516424b0698ca515da236ee9827bd9d107c52d
mm0821 <mari.mueller@uni-tuebingen.de>
2022-06-15 00:11:28 +0200

    Extract only Scala code between mdoc code fences

--- create_scala_files.sh
+++ create_scala_files.sh
@@ -4,9 +4,9 @@
 # ./src directory.
 #
 # The script runs on all markdown files in the ./src directory and deletes
-# everything which isn't inbetween scala codefences, e.g.
+# everything which isn't inbetween scala mdoc codefences, e.g.
 #
-# ```scala
+# ```scala mdoc
 # val x = 1;
 # ```
 #
@@ -14,5 +14,5 @@ echo "Creating .scala files..."
 for d in $(find ./src -name "*.md" -type f ! -name SUMMARY.md ! -name preface.md ! -name furtherreading.md ! -name intro.md ! -name monadic-reflection.md ! -name letcc.md)
 do
   echo "Create .scala file for $d..."
-  sed -n '/```scala/,/```/{/```\(scala\)\?/n;p}' $d > "$(dirname $d)/$(basename -- "$d" .md).scala"
+  sed -n '/```scala\ mdoc/,/```/{/```\(scala\ mdoc\)\?/n;p}' $d > "$(dirname $d)/$(basename -- "$d" .md).scala"
 done
