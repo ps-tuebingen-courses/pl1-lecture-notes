6385c6f92f4e170c27e6ff1caed7f79c57dff094
Linus Szillat <66415456+linususer@users.noreply.github.com>
2023-02-27 21:15:50 +0100

    changed link to dependent git repository
    Link of the dependent git repository changed: https://github.com/se-tuebingen/questionnaire/

--- download_questionnaire_module.sh
+++ download_questionnaire_module.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -ssL -O https://github.com/se-tuebingen/interactive-textbooks/releases/latest/download/questionnaire.js
+curl -ssL -O https://github.com/se-tuebingen/questionnaire/releases/latest/download/questionnaire.js
