20e9d211398cdbf49f4dd02d072fd8a8697292af
klauso <klaus612@gmail.com>
2026-06-22 15:55:02 +0000

    Deploying to gh-pages from @ ps-tuebingen-courses/pl1-lecture-notes@cdd9068eca7fc6c51f0e31e72b6f2c24f922c827 🚀

--- 23-monadic-reflection/monadic-reflection.rkt
+++ 23-monadic-reflection/monadic-reflection.rkt
@@ -1,37 +1,71 @@
 #lang racket
-
 (require racket/control)
 
 (define (return x) (list x))
 (define (bind m f)
-  (apply append (map f m)))
-## Monadic reflection (Filinski)
-; List[A] -> A (quasi-type)
+  (append-map f m))
+
+; List[A] -> A   (quasi-type, see below)
 (define (reflect m)
   (shift k (bind m k)))
 
-; A -> List[A] (quasi-type)
-; this is a macro that transforms (reify e) into (reify-thunk (thunk e))
-; its sole purpose is to prevent the evaluation of e and wrap it into a thunk
-(define-syntaxes reify
-     (syntax-rules ()
-       [(_ e)
-        (reify-thunk (thunk e))]))
-
-(define (reify-thunk t)
-  (reset (return (t))))
+; A -> List[A]   (quasi-type)
+; reify is a macro: it places its argument e textually inside the reset,
+; which both delays e's evaluation and ensures any shift inside e is captured
+; by THIS reset.
+(define-syntax-rule (reify e)
+  (reset (return e)))
 
 (reify (+ (reflect (list 1 2)) (reflect (list 3 4))))
+
+(bind (list 1 2) (lambda (x)
+  (bind (list 3 4) (lambda (y)
+    (return (+ x y))))))
+
+> (reify (+ (reflect (list 1 2)) (reflect (list 3 4))))
+'(4 5 5 6)
+
+(reify (reflect m))  =  m
+
+(reflect (reify e))  =  e
+
+(struct monad (return bind))
+
+(define (reflect M m)
+  (shift k ((monad-bind M) m k)))
+
+(define-syntax-rule (reify M e)
+  (reset ((monad-return M) e)))
+
+(define list-monad
+  (monad (lambda (x) (list x))
+         (lambda (m f) (append-map f m))))
+
+> (reify list-monad
+    (+ (reflect list-monad (list 1 2))
+       (reflect list-monad (list 3 4))))
+'(4 5 5 6)
+
+(define maybe-monad
+  (monad (lambda (x) (cons 'just x))
+         (lambda (m f) (if (eq? m 'nothing) 'nothing (f (cdr m))))))
+
+> (reify maybe-monad
+    (+ (reflect maybe-monad (cons 'just 3))
+       (reflect maybe-monad 'nothing)))
+'nothing
 
 (define (fail) (reflect empty))
 
 ; Nat List[Nat] Nat -> Bool
+; Is row x safe against the already-placed queens in l, the nearest of which
+; sits n columns away?
 (define (safe x l n)
   (or (empty? l)
       (let ((c (first l)))
-        (and (not (= x c))
-             (not (= x (+ c n)))
-             (not (= x (- c n)))
+        (and (not (= x c))            ; same row?
+             (not (= x (+ c n)))      ; same descending diagonal?
+             (not (= x (- c n)))      ; same ascending diagonal?
              (safe x (rest l) (+ n 1))))))
 
 (define (queens n)
@@ -44,3 +78,4 @@
          (inclusive-range 1 n)))
 
 (reify (queens 8))
+
