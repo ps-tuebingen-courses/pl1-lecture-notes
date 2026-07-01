a7bb8c635c98380bd23661910bf0c30f4a189a66
klauso <klaus612@gmail.com>
2026-07-01 17:27:27 +0000

    Deploying to gh-pages from @ ps-tuebingen-courses/pl1-lecture-notes@fddc466a39d00d7f56ce6572c3f1c2cad925be7f 🚀

--- 02-scala-basics/scala-basics.scala
+++ 02-scala-basics/scala-basics.scala
@@ -1,90 +1,77 @@
-val x = 2 + 2;
+val x = 2 + 2
 
-// val für Konstanten
-// def für Methoden
-// var für Variablen
+val y: Int = 2 + 2
+
+def square(n: Int): Int = n * n
+
+square(5)
+
+var counter = 0
+counter = counter + 1
 
 abstract class Person(val name: String) {
-  // String name
-  // val name: String
-
-  def sayHello = {
-    print("hello, " + name)
-  }
+  def sayHello: Unit = print("hello, " + name)
 }
 
 trait ByeBye {
-  def sayGoodbye = {
-    print("bye")
-  }
+  def sayGoodbye: Unit = print("bye")
 }
 
 class Tillmann(name: String) extends Person(name) with ByeBye {
-  override def sayHello = {
-    print("hi")
-  }
+  override def sayHello: Unit = print("hi")
 }
 
 object Me extends Tillmann("Tillmann")
 
+Me.sayHello
 
+sealed trait UniPerson
+case class Student(id: Int) extends UniPerson
+case class Professor(subject: String) extends UniPerson
 
-
-trait UniPerson
-case class Student(val matrikelnummer: Int) extends UniPerson
-case class Professor(val fachgebiet: String) extends UniPerson
-
-object PatternMatching {
-  def showUniPerson(p: UniPerson): String =
-    p match {
-      case Student(m) => "student nr " + m
-      case Professor(f) => "professor on " + f
-    }
-
-  def test = {
-    print(showUniPerson(Student(123)))
-    print(showUniPerson(Professor("programming languages")))
+def describe(p: UniPerson): String =
+  p match {
+    case Student(id)      => "student nr " + id
+    case Professor(field) => "professor of " + field
   }
-}
 
+describe(Student(123))
+describe(Professor("programming languages"))
 
 enum UniPersonEnum:
-  case StudentEnum(val matrikelnummer: Int)
-  case ProfessorEnum(val fachgebiet: String)
+  case StudentEnum(id: Int)
+  case ProfessorEnum(subject: String)
 
-object PatternMatchingEnum {
-  import UniPersonEnum._
+import UniPersonEnum._
 
-  def showUniPerson(p: UniPersonEnum): String =
-    p match {
-      case StudentEnum(m) => "student nr " + m
-      case ProfessorEnum(f) => "professor on " + f
-    }
-
-  def test = {
-    print(showUniPerson(StudentEnum(123)))
-    print(showUniPerson(ProfessorEnum("programming languages")))
+def describeEnum(p: UniPersonEnum): String =
+  p match {
+    case StudentEnum(id)      => "student nr " + id
+    case ProfessorEnum(field) => "professor of " + field
   }
-}
 
+describeEnum(StudentEnum(123))
 
 object AE {
-  // Abstract Syntax Tree
+  // Abstract syntax: an expression is a number, or the addition of two expressions.
   enum Exp:
     case Num(n: Int)
     case Add(lhs: Exp, rhs: Exp)
 
   import Exp._
 
-  // Example
+  // An example expression, standing for 1 + (5 + 3):
   val onePlusEight = Add(Num(1), Add(Num(5), Num(3)))
 
-  // Interpreter
+  // The interpreter: recurse over the structure of the expression.
   def eval(e: Exp): Int =
     e match {
-      case Num(n) => n
-      case Add(lhs, rhs) =>
-        eval(lhs) + eval(rhs)
+      case Num(n)        => n
+      case Add(lhs, rhs) => eval(lhs) + eval(rhs)
     }
 }
+
+AE.onePlusEight
+
+AE.eval(AE.onePlusEight)
 
